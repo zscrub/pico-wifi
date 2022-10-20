ed834e7ad5047cfd373f76cc95ec1596668341c3
Zack <rubinzack40@gmail.com>
2022-10-20 19:53:56 -0400

    api quote -> lcd

--- wifi_companion.py
+++ wifi_companion.py
@@ -3,6 +3,8 @@ from machine import (
     UART
 )
 
+from lcd_init import lcd
+
 uart = UART(0, rx=Pin(1), tx=Pin(0), baudrate=115200)
 
 print(f"Pico connected - {uart}")
@@ -12,13 +14,13 @@ while True:
     while uart.any() > 0:
         try:
             data += uart.read(1)
-            # msg = str(uart.read(), "utf-8", "ignore")
-            # print(msg)
         except Exception as e:
             print(str(e))
             pass
 
     if len(data):
-        # print(data, end="")
         output = data.decode("utf-8")
         print(f"{output}", end="")
+        lcd.clear()
+        lcd.move_to(0, 0)
+        lcd.putstr(output)
