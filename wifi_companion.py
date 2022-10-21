a5995bc42917254fa6e2de0d24dbf8d4f44144b2
Zack <rubinzack40@gmail.com>
2022-10-20 22:31:30 -0400

    handling unicode error on launch sometimes

--- wifi_companion.py
+++ wifi_companion.py
@@ -7,9 +7,9 @@ from driver.lcd_init import lcd
 
 led = Pin(25, Pin.OUT)
 uart = UART(0, rx=Pin(1), tx=Pin(0), baudrate=115200)
-
-print(f"Pico connected - {uart}")
-uart.write("hello")
+msg = f"Pico connected - {uart}"
+print(msg)
+uart.write(msg)
 
 led.toggle()
 while True:
@@ -22,8 +22,11 @@ while True:
             pass
 
     if len(data):
-        output = data.decode("utf-8")
-        print(f"{output}", end="")
-        lcd.clear()
-        lcd.move_to(0, 0)
-        lcd.putstr(output)
+        try:
+            output = data.decode("utf-8")
+            print(f"{output}", end="")
+            lcd.clear()
+            lcd.move_to(0, 0)
+            lcd.putstr(output)
+        except UnicodeError as e:
+            pass
